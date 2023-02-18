38bec7512549432558a79eb739c785b8c07f62c2
yasuwo <y.honjo.i@gmail.com>
2023-02-19 02:33:24 +0900

    change columns and color scale

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, {useState, useEffect} from "react";
 import MapChart from "./MapChart"
+import SampleColor from "./SampleColor"
 import {csv} from "d3-fetch";
 import {csvParse} from "d3-dsv";
 
@@ -13,38 +14,65 @@ const App = () => {
 	const [data, setData] = useState({});
 	const [year, setYY] = useState(2022);
 	const [month, setMM] = useState(1);
-	const [day, _setDD] = useState(1);
-	const [active_col, set_active_col] = useState(1);
-	const [col, setcol] = useState([]);
+	const day = 1;
+	const [activeCol, setActiveCol] = useState(1);
+	const [col, setCol] = useState([]);
 	const [filter_day, setFilter] = useState(setyyyymmdd(2022, 1, 1));
 	const [file, setFile] = useState();
+	const [file_name, setFileName] = useState("");
 	const [min_value, setMinValue] = useState(0);
 	const [max_value, setMaxValue] = useState(10);
 	const [min_color, setMinColor] = useState("#FFFFFF");
 	const [max_color, setMaxColor] = useState("#FF0000");
+	const [has_row_id, setHasrowid] = useState(true);
 	const fileReader = new FileReader();
 
 	const handleOnChange = (e) => {
 		setFile(e.target.files[0]);
 	};
-	const set_values = (array) => {
-		let contents = array.map(elem => {
-			let id = Number(elem.row_id.split("_")[0]);
-			let yyyymmdd = elem.row_id.split("_")[1];
-			return Object.assign({id: id, yyyymmdd: yyyymmdd}, elem);
-		});
-		let ymd = new Set(contents.map(elem => elem.yyyymmdd));
-		for (const x of ymd) {
-			data[x] = contents.filter(v => v.yyyymmdd == x);
+	const setValues = (array) => {
+		if ("row_id" in array[0]) {
+			setHasrowid(true);
+			const contents = array.map(elem => {
+				const cfips = Number(elem.row_id.split("_")[0]);
+				const first_day_of_month = elem.row_id.split("_")[1];
+				return Object.assign({cfips: cfips, first_day_of_month: first_day_of_month}, elem);
+			});
+			const yymd = new Set(contents.map(elem => elem.first_day_of_month));
+			for (const x of yymd) {
+				data[x] = contents.filter(v => v.first_day_of_month === x);
+			}
+			setData(data);
 		}
-		setcol(Object.keys(array[0]));
-		set_active_col(Object.keys(array[0]).length - 1);
-		setData(data);
+		else if ("cfips" in array[0]) {
+			if ("first_day_of_month" in array[0]) {
+				setHasrowid(true);
+				const yymd = new Set(array.map(elem => elem.first_day_of_month));
+				for (const x of yymd) {
+					data[x] = array.filter(v => v.first_day_of_month === x);
+				}
+				setData(data);
+			}
+			else {
+				setHasrowid(false);
+				data["!"] = array;
+				setData(data);
+			}
+		}
+		else {
+			setHasrowid(false);
+		}
+		setCol(Object.keys(array[0]));
+		let colindex = Object.keys(array[0]).indexOf("microbusiness_density");
+		if (colindex < 0) {
+			colindex = Object.keys(array[0]).length - 1;
+		}
+		setActiveCol(colindex);
 	};
 
 	const csvFiletoData = (csvdata) => {
 		const array = csvParse(csvdata);
-		set_values(array);
+		setValues(array);
 	};
 
 	const handleOnSubmit = (e) => {
@@ -56,137 +84,172 @@ const App = () => {
 				csvFiletoData(text);
 			};
 
+			setFileName(file.name);
 			fileReader.readAsText(file);
 		}
 	};
 
 	useEffect(() => {
-		setFilter(setyyyymmdd(year, month, day));
-		csv("train_short.csv").then(counties => {
-			set_values(counties);
+		const filename = "train_short.csv";
+		csv(filename).then(counties => {
+			setValues(counties);
 		});
+		setFileName(filename);
 	}, []);
 
+	const YearButton = (
+		<div className="input-group mt-1">
+			<label className="input-group-text">
+				Year
+			</label>
+			<input value={year} disabled></input>
+			<button className="btn btn-outline-primary px-3" onClick={() => {setYY(v => v - 1); setFilter(setyyyymmdd(year - 1, month, day));}}>
+				-Y
+			</button>
+			<button className="btn btn-outline-primary px-3" onClick={() => {setYY(v => v + 1); setFilter(setyyyymmdd(year + 1, month, day));}}>
+				+Y
+			</button>
+		</div>
+	);
+	const MonthButton = (
+		<div className="input-group mt-1">
+			<label className="input-group-text">
+				Month
+			</label>
+			<input value={month} disabled></input>
+			<button className="btn btn-outline-primary px-3" onClick={() => {
+				if (month === 1) {
+					setYY(v => v - 1);
+					setMM(12);
+					setFilter(setyyyymmdd(year - 1, 12, day));
+				}
+				else {
+					setMM(v => v - 1);
+					setFilter(setyyyymmdd(year, month - 1, day));
+				}
+			}}>
+				-M
+			</button>
+			<button className="btn btn-outline-primary px-3" onClick={() => {
+				if (month === 12) {
+					setYY(v => v + 1);
+					setMM(1);
+					setFilter(setyyyymmdd(year + 1, 1, day));
+				}
+				else {
+					setMM(v => v + 1);
+					setFilter(setyyyymmdd(year, month + 1, day));
+				}
+			}}>
+				+M
+			</button>
+		</div>
+	);
+
+	const ImportButton = (
+		<div className="input-group mt-4 w-50">
+			<input type={"file"} className="form-control" accept={".csv"} onChange={handleOnChange} />
+			<button className="btn btn-outline-primary px-4"
+				onClick={(e) => {
+					handleOnSubmit(e);
+				}} t
+			>
+				IMPORT CSV
+			</button>
+		</div>
+
+	);
+
+	const FileName = (
+		< div >
+			{file_name}
+		</div >
+	);
+
+	const ColPanel = (
+		<div>
+			{col.map(elem => {
+				if (elem === col[activeCol]) {
+					return <span style={{fontWeight: "bold"}}> [{elem}] </span>
+				}
+				else {
+					return <span> {elem} </span>
+				}
+			}
+			)}
+		</div >
+	);
+
+	const ColButton = (
+		<div className="input-group mt-1">
+			<label className="input-group-text">
+				column
+			</label>
+			<input value={col[activeCol]} disabled></input>
+			<button className="btn btn-outline-primary px-3" onClick={() => {
+				if (activeCol > 0) {
+					setActiveCol(v => v - 1);
+				}
+			}}>
+				←
+			</button>
+			<button className="btn btn-outline-primary px-3" onClick={() => {
+				if (activeCol + 1 < col.length) {
+					setActiveCol(v => v + 1);
+				}
+			}}>
+				→
+			</button>
+		</div>
+	);
+	const MinButton = (
+		<div className="input-group mt-1">
+			<label className="input-group-text">
+				min
+			</label>
+			<input value={min_value} onChange={(e) => {setMinValue(e.target.value)}}></input>
+			<label className="input-group-text">
+				min_color
+			</label>
+			<input type="color" value={min_color} onChange={(e) => {setMinColor(e.target.value)}}></input>
+
+		</div>
+	);
+
+	const MaxButton = (
+		<div className="input-group mt-1">
+			<label className="input-group-text">
+				Max
+			</label>
+			<input value={max_value} onChange={(e) => {setMaxValue(e.target.value)}}></input>
+			<label className="input-group-text">
+				Max_color
+			</label>
+			<input type="color" value={max_color} onChange={(e) => {setMaxColor(e.target.value)}}></input>
+		</div >
+	);
+
+	const YMDPanel = (
+		<div>
+			{year}-{month}-{day} / {filter_day}
+		</div>
+	);
 
 	return (
 		<div>
-			<div>
-				<div className="input-group mt-4 w-50">
-					<label htmlFor="csv" className="input-group-text">Update submission.csv:</label>
-					<input type={"file"} className="form-control" accept={".csv"} onChange={handleOnChange} />
-					<button className="btn btn-outline-primary px-4"
-						onClick={(e) => {
-							handleOnSubmit(e);
-						}}
-					>
-						IMPORT CSV
-					</button>
-				</div>
-				<div>
-					<div className="input-group mt-1">
-						<label className="input-group-text">
-							Year
-						</label>
-						<button className="btn btn-outline-primary px-3" onClick={() => {setYY(v => v + 1); setFilter(setyyyymmdd(year + 1, month, day));}}>
-							+Y
-						</button>
-						<button className="btn btn-outline-primary px-3" onClick={() => {setYY(v => v - 1); setFilter(setyyyymmdd(year - 1, month, day));}}>
-							-Y
-						</button>
-					</div>
+			{ImportButton}
 
-					<div className="input-group mt-1">
-						<label className="input-group-text">
-							Month
-						</label>
-						<button className="btn btn-outline-primary px-3" onClick={() => {
-							if (month == 12) {
-								setYY(v => v + 1);
-								setMM(1);
-								setFilter(setyyyymmdd(year + 1, 1, day));
-							}
-							else {
-								setMM(v => v + 1);
-								setFilter(setyyyymmdd(year, month + 1, day));
-							}
-						}}>
-							+M
-						</button>
-						<button className="btn btn-outline-primary px-3" onClick={() => {
-							if (month == 1) {
-								setYY(v => v - 1);
-								setMM(12);
-								setFilter(setyyyymmdd(year - 1, 12, day));
-							}
-							else {
-								setMM(v => v - 1);
-								setFilter(setyyyymmdd(year, month - 1, day));
-							}
-						}}>
-							-M
-						</button>
-					</div>
-					<div className="panel panel-default">
-						<div className="panel-body">
-							{year}-{month}-{day} / {filter_day}
-						</div>
-					</div>
-					<div>
-						{col.map(elem => {
-							if (elem === col[active_col]) {
-								return <span stype="background:rgb(255,0,0);">[{elem}] </span>
-							}
-							else {
-								return <span>{elem} </span>
-							}
-						}
-						)}
-						<div className="input-group mt-1">
-							<label className="input-group-text">
-								columns
-							</label>
-							<button className="btn btn-outline-primary px-3" onClick={() => {
-								if (active_col > 0) {
-									set_active_col(v => v - 1);
-								}
-							}}>
-								←
-							</button>
-							<button className="btn btn-outline-primary px-3" onClick={() => {
-								if (active_col + 1 < col.length) {
-									set_active_col(v => v + 1);
-								}
-							}}>
-								→
-							</button>
-						</div>
-						<div className="input-group mt-1">
-							<label className="input-group-text">
-								min
-							</label>
-							<input value={min_value} onChange={(e) => {setMinValue(Number(e.target.value))}}></input>
-							<label className="input-group-text">
-								min_color
-							</label>
-							<input type="color" value={min_color} onChange={(e) => {setMinColor(e.target.value)}}></input>
+			{FileName}
+			{ColPanel}
+			{YMDPanel}
+			{MinButton}
+			{MaxButton}
 
-						</div>
-						<div className="input-group mt-1">
-							<label className="input-group-text">
-								Max
-							</label>
-							<input value={max_value} onChange={(e) => {setMaxValue(Number(e.target.value))}}></input>
-							<label className="input-group-text">
-								Max_color
-							</label>
-							<input type="color" value={max_color} onChange={(e) => {setMaxColor(e.target.value)}}></input>
-						</div>
-					</div>
-				</div>
-			</div>
-			<MapChart data={data[filter_day]} col={col[active_col]} min_value={min_value} max_value={max_value} min_color={min_color} max_color={max_color} key="us-state" />
-			<a href="https://www.kaggle.com/competitions/godaddy-microbusiness-density-forecasting" target="_blank">←back to kaggle</a>,
-			<a href="https://github.com/HNJ755329/godaddy_visualizer" target="_blank">github</a>
+			{ColButton}
+			{YearButton}
+			{MonthButton}
+			<SampleColor col={col[activeCol]} min_value={min_value} max_value={max_value} min_color={min_color} max_color={max_color} key="sample-color" />
+			<MapChart data={has_row_id ? data[filter_day] : data["!"]} col={col[activeCol]} min_value={min_value} max_value={max_value} min_color={min_color} max_color={max_color} key="us-state" />
 		</div >
 	);
 }
--- src/MapChart.js
+++ src/MapChart.js
@@ -9,25 +9,14 @@ const MapChart = (props) => {
 		.domain([props.min_value, props.max_value])
 		.range([props.min_color, props.max_color])
 
-	let sample_colors = new Array();
-	for (let i = 0; i <= 10; i++) {
-		const value = Number(props.min_value) + i * (Number(props.max_value) - Number(props.min_value)) / 10;
-		sample_colors.push(<span style={{background: colorScale(value)}
-		} > {Number(value)} </span >);
-
-	}
-
 	return (
 		<>
-			{props.col} :
-			{sample_colors}
-
 			<ComposableMap projection="geoAlbersUsa">
 				<Geographies geography={geoUrl}>
 					{({geographies}) =>
 						geographies.map(geo => {
 							if (props.data) {
-								const cur = props.data.find(s => s.id == geo.id);
+								const cur = props.data.find(s => Number(s.cfips) === Number(geo.id));
 								return (
 									<Geography
 										key={geo.rsmKey}
